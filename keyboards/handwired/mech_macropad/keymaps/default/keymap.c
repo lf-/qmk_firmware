32372b33d14bcecaa4c0616be43a37c54b088944
lf- <lf-@users.noreply.github.com>
2020-01-20 21:19:45 -0800

    ++project_gay_coefficient

--- keyboards/handwired/mech_macropad/keymaps/default/keymap.c
+++ keyboards/handwired/mech_macropad/keymaps/default/keymap.c
@@ -19,7 +19,7 @@
 enum my_layers { _NUMPAD, _MACROS, _SUDOKU };
 
 // Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes { AUTOCLICKER = SAFE_RANGE, QMKURL };
+enum custom_keycodes { AUTOCLICKER = SAFE_RANGE, RGB_BRIGHTER, RGB_DIMMER };
 static bool autoclicker_on = false;
 
 #define NOTIFY_CURR_TRACK LCTL(KC_F23)
@@ -38,8 +38,8 @@ static bool autoclicker_on = false;
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUMPAD] = LAYOUT(
-        TO(_MACROS),	BRIGHTUP,	RGB_VAI,	KC_NO,
-        SCREENOFF,	BRIGHTDN,	RGB_VAD,	AUTOCLICKER,
+        TO(_MACROS),	BRIGHTUP,	RGB_BRIGHTER,	KC_NO,
+        SCREENOFF,	BRIGHTDN,	RGB_DIMMER,	AUTOCLICKER,
 
         KC_NUMLOCK,	KC_KP_SLASH,	KC_KP_ASTERISK,	KC_KP_MINUS,
         KC_KP_7,	KC_KP_8,	KC_KP_9,	KC_KP_PLUS,
@@ -48,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_KP_0,	KC_KP_0,	KC_KP_DOT,	KC_KP_ENTER
     ),
     [_MACROS] = LAYOUT(
-        TO(_SUDOKU),	BRIGHTUP,	RGB_VAI,	RESET,
-        SCREENOFF,	BRIGHTDN,		RGB_VAD,	AUTOCLICKER,
+        TO(_SUDOKU),	BRIGHTUP,	RGB_BRIGHTER,	RESET,
+        SCREENOFF,	BRIGHTDN,	RGB_DIMMER,	AUTOCLICKER,
 
         MIC_MUTE,	KC_NO,		KC_NO,		KC_NO,
         OBS1,		OBS2,		OBS3,		OBS4,
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MPRV,	KC_MPLY,	KC_MNXT,	DISLIKE_CURR_TRACK
     ),
     [_SUDOKU] = LAYOUT(
-        TO(_NUMPAD),	BRIGHTUP,	RGB_VAI,	KC_NO,
-        SCREENOFF,	BRIGHTDN,	RGB_VAD,	AUTOCLICKER,
+        TO(_NUMPAD),	BRIGHTUP,	RGB_BRIGHTER,	KC_NO,
+        SCREENOFF,	BRIGHTDN,	RGB_DIMMER,	AUTOCLICKER,
 
         KC_LEFT,	KC_DOWN,	KC_UP,		KC_RIGHT,
         KC_7,		KC_8,		KC_9,		KC_SPACE,
@@ -76,17 +76,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         // on press
         autoclicker_on = !autoclicker_on;
-	layer_state_set_user(layer_state);
+        layer_state_set_user(layer_state);
       } else {
         // on release
       }
       break;
-    case QMKURL:
+    case RGB_BRIGHTER:
       if (record->event.pressed) {
-        // when keycode QMKURL is pressed
-        SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
-      } else {
-        // when keycode QMKURL is released
+        rgblight_increase_val();
+        layer_state_set_user(layer_state);
+      }
+      break;
+    case RGB_DIMMER:
+      if (record->event.pressed) {
+        rgblight_decrease_val();
+        layer_state_set_user(layer_state);
       }
       break;
   }
@@ -111,7 +115,7 @@ void matrix_scan_user(void) {
     repeat_click = delay_click = 0;
     return;
   }
-  
+
   if (delay_click && timer_elapsed(delay_click) > 16) {
     unregister_code(KC_MS_BTN1);
     delay_click = 0;
@@ -123,16 +127,47 @@ void matrix_scan_user(void) {
 
 void led_set_user(uint8_t usb_led) {}
 
+/**
+ * Set hue to -1 to turn off the LED
+ */
+typedef struct {
+  uint16_t hue;
+  uint8_t sat;
+} Colour;
+
+#define NUM_COLOURS 5
+
+Colour colour_patterns[][NUM_COLOURS] = {
+  {{197, 100}, {348, 100}, {0, 0}, {348, 100}, {197, 100}},  /* trans flag */
+  {{57, 100}, {0, 0}, {273, 100}, {244, 100}, {-1, 0}},     /* nonbinary flag */
+  {{267, 100}, {0, 0}, {85, 100}, {-1, 0}, {-1, 0}},       /* queer flag */
+};
+
+void set_colour_pattern(Colour pattern[NUM_COLOURS]) {
+  for (uint8_t i = 0; i < NUM_COLOURS; ++i) {
+    Colour col = pattern[i];
+    // turn off colours with -1 hue (sentinel value)
+    if (col.hue == -1) {
+      rgblight_setrgb_at(0, 0, 0, i);
+      continue;
+    }
+    rgblight_sethsv_at(col.hue, col.sat, rgblight_get_val(), i);
+  }
+}
+
 uint32_t layer_state_set_user(uint32_t state) {
   switch (biton32(state)) {
     case _NUMPAD:
-      rgb_seths(53, 255);
+      set_colour_pattern(colour_patterns[0]);
+      //rgb_seths(53, 255);
       break;
     case _MACROS:
-      rgb_seths(0, 255);
+      set_colour_pattern(colour_patterns[1]);
+      //rgb_seths(0, 255);
       break;
     case _SUDOKU:
-      rgb_seths(224, 63);
+      set_colour_pattern(colour_patterns[2]);
+      //rgb_seths(224, 63);
       break;
     default:
       rgb_seths(0, 0);
@@ -140,8 +175,6 @@ uint32_t layer_state_set_user(uint32_t state) {
   }
   if (autoclicker_on) {
     rgblight_setrgb_at(255, 255, 255, 4);
-  } else {
-    rgb_seths(rgblight_get_hue(), rgblight_get_sat());
   }
   return state;
 }
