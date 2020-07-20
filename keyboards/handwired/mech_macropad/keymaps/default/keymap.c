a5bf83e7e6fe7db54676e68c8013c061927c7f61
lf- <lf-@users.noreply.github.com>
2020-07-20 00:21:49 -0700

    port colour code from dz60

--- keyboards/handwired/mech_macropad/keymaps/default/keymap.c
+++ keyboards/handwired/mech_macropad/keymaps/default/keymap.c
@@ -7,11 +7,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.    If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
 #include "tmk_core/common/timer.h"
@@ -37,78 +37,115 @@ static bool autoclicker_on = false;
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-        [_NUMPAD] = LAYOUT(
-                TO(_MACROS),	BRIGHTUP,	RGB_BRIGHTER,	KC_NO,
-                SCREENOFF,	BRIGHTDN,	RGB_DIMMER,	AUTOCLICKER,
+	[_NUMPAD] = LAYOUT(
+		TO(_MACROS),	BRIGHTUP,	RGB_BRIGHTER,	KC_NO,
+		SCREENOFF,	BRIGHTDN,	RGB_DIMMER,	AUTOCLICKER,
 
-                KC_NUMLOCK,	KC_KP_SLASH,	KC_KP_ASTERISK,	KC_KP_MINUS,
-                KC_KP_7,	KC_KP_8,	KC_KP_9,	KC_KP_PLUS,
-                KC_KP_4,	KC_KP_5,	KC_KP_6,	KC_KP_PLUS,
-                KC_KP_1,	KC_KP_2,	KC_KP_3,	KC_KP_ENTER,
-                KC_KP_0,	KC_KP_0,	KC_KP_DOT,	KC_KP_ENTER
-        ),
-        [_MACROS] = LAYOUT(
-                TO(_SUDOKU),	BRIGHTUP,	RGB_BRIGHTER,	RESET,
-                SCREENOFF,	BRIGHTDN,	RGB_DIMMER,	AUTOCLICKER,
+		KC_NUMLOCK,	KC_KP_SLASH,	KC_KP_ASTERISK,	KC_KP_MINUS,
+		KC_KP_7,	KC_KP_8,	KC_KP_9,	KC_KP_PLUS,
+		KC_KP_4,	KC_KP_5,	KC_KP_6,	KC_KP_PLUS,
+		KC_KP_1,	KC_KP_2,	KC_KP_3,	KC_KP_ENTER,
+		KC_KP_0,	KC_KP_0,	KC_KP_DOT,	KC_KP_ENTER
+	),
+	[_MACROS] = LAYOUT(
+		TO(_SUDOKU),	BRIGHTUP,	RGB_BRIGHTER,	RESET,
+		SCREENOFF,	BRIGHTDN,	RGB_DIMMER,	AUTOCLICKER,
 
-                MIC_MUTE,	KC_NO,		KC_NO,		KC_NO,
-                OBS1,		OBS2,		OBS3,		OBS4,
-                KC_NO,		KC_NO,		KC_NO,		NOTIFY_CURR_TRACK,
-                KC_VOLD,	KC_MUTE,	KC_VOLU,	LIKE_CURR_TRACK,
-                KC_MPRV,	KC_MPLY,	KC_MNXT,	DISLIKE_CURR_TRACK
-        ),
-        [_SUDOKU] = LAYOUT(
-                TO(_NUMPAD),	BRIGHTUP,	RGB_BRIGHTER,	KC_NO,
-                SCREENOFF,	BRIGHTDN,	RGB_DIMMER,	AUTOCLICKER,
+		MIC_MUTE,	KC_NO,		KC_NO,		KC_NO,
+		OBS1,		OBS2,		OBS3,		OBS4,
+		KC_NO,		KC_NO,		KC_NO,		NOTIFY_CURR_TRACK,
+		KC_VOLD,	KC_MUTE,	KC_VOLU,	LIKE_CURR_TRACK,
+		KC_MPRV,	KC_MPLY,	KC_MNXT,	DISLIKE_CURR_TRACK
+	),
+	[_SUDOKU] = LAYOUT(
+		TO(_NUMPAD),	BRIGHTUP,	RGB_BRIGHTER,	KC_NO,
+		SCREENOFF,	BRIGHTDN,	RGB_DIMMER,	AUTOCLICKER,
 
-                KC_LEFT,	KC_DOWN,	KC_UP,		KC_RIGHT,
-                KC_7,		KC_8,		KC_9,		KC_SPACE,
-                KC_4,		KC_5,		KC_6,		KC_DEL,
-                KC_1,		KC_2,		KC_3,	 	KC_ESC,
-                OSM(MOD_LSFT),	OSM(MOD_LCTL),	LCTL(KC_Z),	LCTL(KC_Y)
-        ),
+		KC_LEFT,	KC_DOWN,	KC_UP,		KC_RIGHT,
+		KC_7,		KC_8,		KC_9,		KC_SPACE,
+		KC_4,		KC_5,		KC_6,		KC_DEL,
+		KC_1,		KC_2,		KC_3,		KC_ESC,
+		OSM(MOD_LSFT),	OSM(MOD_LCTL),	LCTL(KC_Z),	LCTL(KC_Y)
+	),
 };
 // clang-format on
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case AUTOCLICKER:
-            if (record->event.pressed) {
-                // on press
-                autoclicker_on = !autoclicker_on;
-                layer_state_set_user(layer_state);
-            } else {
-                // on release
-            }
-            break;
-        case RGB_BRIGHTER:
-            if (record->event.pressed) {
-                rgblight_increase_val();
-                layer_state_set_user(layer_state);
-            }
-            break;
-        case RGB_DIMMER:
-            if (record->event.pressed) {
-                rgblight_decrease_val();
-                layer_state_set_user(layer_state);
-            }
-            break;
+void led_set_user(uint8_t usb_led) {}
+
+/**
+ * Set hue to -1 to turn off the LED
+ */
+typedef struct {
+    int16_t hue;
+    uint8_t sat;
+} Colour;
+
+typedef union {
+    uint32_t raw;
+    struct {
+        uint8_t val;
+    };
+} user_config_t;
+
+user_config_t user_config;
+
+#define NUM_COLOURS 5
+
+#define VAL_STEP 10
+#define OFF {-1, 0}
+// Convert colours from degrees and 0-100 sat as is used in most image editors
+// to byte distance around a circle and 0-255 sat
+#define CLR(hue, sat) {(uint8_t) (hue / 360.0 * 255.0), (uint8_t) (sat / 100.0 * 255.0)}
+
+Colour colour_patterns[][NUM_COLOURS] = {
+    {CLR(197, 100), CLR(348, 100), CLR(0, 0), CLR(348, 100), CLR(197, 100)},    /* trans flag */
+    {CLR(57, 100), CLR(0, 0), CLR(273, 100), CLR(244, 100), OFF},         /* nonbinary flag */
+    {CLR(267, 100), CLR(0, 0), CLR(85, 100), OFF, OFF},             /* queer flag */
+};
+
+void set_colour_pattern(Colour pattern[NUM_COLOURS]) {
+    for (uint8_t i = 0; i < NUM_COLOURS; ++i) {
+        Colour col = pattern[i];
+        // turn off colours with -1 hue (sentinel value)
+        if (col.hue == -1) {
+            setrgb(0, 0, 0, &led[i]);
+            continue;
+        }
+
+        uint8_t val = user_config.val;
+        sethsv(col.hue, col.sat, val, &led[i]);
     }
-    return true;
+    rgblight_set();
 }
 
-void matrix_init_user(void) {}
-
-void rgb_seths(uint16_t hue, uint8_t sat) { rgblight_sethsv_noeeprom(hue, sat, rgblight_get_val()); }
-
-void incr
-
-void keyboard_post_init_user(void) {
-    rgblight_enable_noeeprom()();
-    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-    refresh_colour_pattern();
+void refresh_colour_pattern(void) {
     layer_state_set_user(layer_state);
-    debug_enable = true;
+}
+
+void save_user_config(void) {
+    eeconfig_update_user(user_config.raw);
+}
+
+void decrease_val(void) {
+    uint8_t new_val = user_config.val - VAL_STEP;
+    if (new_val > user_config.val) {
+        // overflowed, unsigned overflow is defined
+        new_val = 0;
+    }
+    user_config.val = new_val;
+}
+
+void increase_val(void) {
+    uint8_t new_val = user_config.val + VAL_STEP;
+    if (new_val < user_config.val) {
+        // overflowed, unsigned overflow is defined
+        new_val = 255;
+    }
+    user_config.val = new_val;
+}
+
+void matrix_init_user(void) {
+    user_config.raw = eeconfig_read_user();
 }
 
 void matrix_scan_user(void) {
@@ -128,39 +165,6 @@ void matrix_scan_user(void) {
     }
 }
 
-void led_set_user(uint8_t usb_led) {}
-
-/**
- * Set hue to -1 to turn off the LED
- * Actual range of hue is 0~255
- */
-typedef struct {
-    int16_t hue;
-    uint8_t sat;
-} Colour;
-
-#define NUM_COLOURS 5
-#define OFF {-1, 0}
-#define CLR(hue, sat) {(uint8_t) (hue / 360.0 * 255.0), (uint8_t) (sat / 100.0 * 255.0)}
-
-Colour colour_patterns[][NUM_COLOURS] = {
-    {CLR(197, 100), CLR(348, 100), CLR(0, 0), CLR(348, 100), CLR(197, 100)},    /* trans flag */
-    {CLR(57, 100), CLR(0, 0), CLR(273, 100), CLR(244, 100), OFF},         /* nonbinary flag */
-    {CLR(267, 100), CLR(0, 0), CLR(85, 100), OFF, OFF},             /* queer flag */
-};
-
-void set_colour_pattern(Colour pattern[NUM_COLOURS]) {
-    for (uint8_t i = 0; i < NUM_COLOURS; ++i) {
-        Colour col = pattern[i];
-        // turn off colours with -1 hue (sentinel value)
-        if (col.hue == -1) {
-            rgblight_setrgb_at(0, 0, 0, i);
-            continue;
-        }
-        rgblight_sethsv_at(col.hue, col.sat, rgblight_get_val(), i);
-    }
-}
-
 uint32_t layer_state_set_user(uint32_t state) {
     switch (biton32(state)) {
         case _NUMPAD:
@@ -176,11 +180,42 @@ uint32_t layer_state_set_user(uint32_t state) {
             //rgb_seths(224, 63);
             break;
         default:
-            rgb_seths(0, 0);
+            sethsv(0, 0, 100, &led[0]);
             break;
     }
     if (autoclicker_on) {
-        rgblight_setrgb_at(255, 255, 255, 4);
+        setrgb(255, 255, 255, &led[4]);
     }
+    rgblight_set();
     return state;
 }
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case AUTOCLICKER:
+            if (record->event.pressed) {
+                // on press
+                autoclicker_on = !autoclicker_on;
+                refresh_colour_pattern();
+            } else {
+                // on release
+            }
+            break;
+        case RGB_BRIGHTER:
+            if (record->event.pressed) {
+                increase_val();
+                refresh_colour_pattern();
+                save_user_config();
+            }
+            break;
+        case RGB_DIMMER:
+            if (record->event.pressed) {
+                decrease_val();
+                refresh_colour_pattern();
+                save_user_config();
+            }
+            break;
+    }
+    return true;
+}
+
